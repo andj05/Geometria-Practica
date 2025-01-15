a390186ecaec8c5288adb50bb535ee36feaddeb3
Anddy <jara08611@gmail.com>
2025-01-15 17:58:17 -0400

    DiagramaAdd project files.

---
+++ Geometria/Class/Rectangle.cs
@@ -0,0 +1,15 @@
+﻿namespace GeometriaApp
+{
+    // Esta clase hereda de la clase abstracta Shape
+    public class Rectangle : Shape
+    {
+        // Constructor que pasa los valores al constructor de la clase base
+        public Rectangle(double ancho, double alto) : base(ancho, alto) { }
+
+        // Implementación del método abstracto para calcular el área del rectángulo
+        public override double CalculateSurface()
+        {
+            return Ancho * Alto;  // Área = Ancho * Alto
+        }
+    }
+}
